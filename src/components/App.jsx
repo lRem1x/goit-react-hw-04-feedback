f84bd4762a070edf9510ab5c23a25fb20402849b
Roman Balakleec <lrem1x777@gmail.com>
2023-02-27 00:26:20 +0200

    setup

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,14 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from 'react';
+import FeedbackForm from './FeedbackForm/';
+
+export class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  render() {
+    return <FeedbackForm state={this.state}></FeedbackForm>;
+  }
+}
